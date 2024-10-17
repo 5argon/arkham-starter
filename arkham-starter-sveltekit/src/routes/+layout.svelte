dd0d5fd2220c83005925570f6c99ea65701c1213
Sirawat Pitaksarit <pyasry@gmail.com>
2024-10-17 12:23:40 +0700

    big

--- arkham-starter-sveltekit/src/routes/+layout.svelte
+++ arkham-starter-sveltekit/src/routes/+layout.svelte
@@ -12,16 +12,3 @@
 </main>
 
 <Footer />
-
-<style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1400px;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-</style>
