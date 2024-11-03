3599929845a1b6c71a01fe40e8e207bccb6d4e71
Sirawat Pitaksarit <pyasry@gmail.com>
2024-11-04 01:52:30 +0700

    expansion display font size decrease

--- arkham-starter-sveltekit/src/lib/design/components/expansion/ExpansionDisplay.svelte
+++ arkham-starter-sveltekit/src/lib/design/components/expansion/ExpansionDisplay.svelte
@@ -1,39 +1,39 @@
-<script lang='ts'>
-  import { goto } from '$app/navigation'
-  import type { CardPack } from '$lib/core/card-pack'
-  import {
-    CardPack,
-    CardPackIconColor,
-    getCampaignStaticUrl,
-    getPackStaticUrl,
-  } from '$lib/design/interface/card-pack'
+<script lang="ts">
+	import { goto } from '$app/navigation'
+	import type { CardPack } from '$lib/core/card-pack'
+	import {
+		CardPack,
+		CardPackIconColor,
+		getCampaignStaticUrl,
+		getPackStaticUrl,
+	} from '$lib/design/interface/card-pack'
 
-  import { packToFile } from './pack-to-file'
+	import { packToFile } from './pack-to-file'
 
-  export let set: CardPack
-  export let text: string
-  export let subText: string | undefined = undefined
-  export let link: string | undefined = undefined
-  export let onClick: ((cardPack: CardPack) => void) | undefined = undefined
-  export let campaign: boolean = false
-  export let disabled: boolean = false
+	export let set: CardPack
+	export let text: string
+	export let subText: string | undefined = undefined
+	export let link: string | undefined = undefined
+	export let onClick: ((cardPack: CardPack) => void) | undefined = undefined
+	export let campaign: boolean = false
+	export let disabled: boolean = false
 
-  let backgroundImageString: string = ''
-  $: {
-    const file = packToFile(set)
-    if (file && !disabled) {
-      backgroundImageString = `background-image: url(/image/expansion/${campaign ? 'campaign' : 'investigator'}/${file}.webp);`
-    } else {
-      backgroundImageString = ''
-    }
-  }
+	let backgroundImageString: string = ''
+	$: {
+		const file = packToFile(set)
+		if (file && !disabled) {
+			backgroundImageString = `background-image: url(/image/expansion/${campaign ? 'campaign' : 'investigator'}/${file}.webp);`
+		} else {
+			backgroundImageString = ''
+		}
+	}
 </script>
 
 <button
-  class='frame'
-  class:frame-disabled={disabled}
-  style={backgroundImageString}
-  on:click={() => {
+	class="frame"
+	class:frame-disabled={disabled}
+	style={backgroundImageString}
+	on:click={() => {
 		if (onClick) {
 			onClick(set)
 		} else if (link) {
@@ -41,89 +41,93 @@
 		}
 	}}
 >
-		<span class='overlay-color'>
-			<div class='inner-frame' class:inner-frame-disabled={disabled}>
-				<div class='flex-icon-text'>
-					<img
-            class='pack-icon'
-            alt='pack icon'
-            src={campaign
-							? getCampaignStaticUrl(set, disabled ? CardPackIconColor.Black :  CardPackIconColor.White)
-							: getPackStaticUrl(set, disabled? CardPackIconColor.Black : CardPackIconColor.White)}
-          />
-					<div class:disabled-text={disabled}>
-						<div class='text'>{text}</div>
-            {#if subText}
-							<div class='sub-text'>{subText}</div>
-						{/if}
-					</div>
+	<span class="overlay-color">
+		<div class="inner-frame" class:inner-frame-disabled={disabled}>
+			<div class="flex-icon-text">
+				<img
+					class="pack-icon"
+					alt="pack icon"
+					src={campaign
+						? getCampaignStaticUrl(
+								set,
+								disabled ? CardPackIconColor.Black : CardPackIconColor.White,
+							)
+						: getPackStaticUrl(set, disabled ? CardPackIconColor.Black : CardPackIconColor.White)}
+				/>
+				<div class:disabled-text={disabled}>
+					<div class="text">{text}</div>
+					{#if subText}
+						<div class="sub-text">{subText}</div>
+					{/if}
 				</div>
 			</div>
-		</span>
+		</div>
+	</span>
 </button>
 
 <style>
-    .frame {
-        border: 1px solid black;
-        margin: 2px 2px;
-        padding: 0;
-        background-size: cover;
-        background-color: rgba(0, 0, 0, 0);
-    }
+	.frame {
+		border: 1px solid black;
+		margin: 2px 2px;
+		padding: 0;
+		background-size: cover;
+		background-color: rgba(0, 0, 0, 0);
+	}
 
-    .frame-disabled {
-        background-image: none;
-    }
+	.frame-disabled {
+		background-image: none;
+	}
 
-    .frame:hover {
-        cursor: pointer;
-    }
+	.frame:hover {
+		cursor: pointer;
+	}
 
-    .overlay-color {
-        color: white;
-        width: 230px;
-        height: 50px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-    }
+	.overlay-color {
+		color: white;
+		width: 230px;
+		height: 50px;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
 
-    .overlay-color:hover {
-        background-color: rgba(255, 255, 255, 0.2);
-    }
+	.overlay-color:hover {
+		background-color: rgba(255, 255, 255, 0.2);
+	}
 
-    .inner-frame {
-        background-color: rgba(0, 0, 0, 0.7);
-        padding: 0px 6px;
-    }
+	.inner-frame {
+		background-color: rgba(0, 0, 0, 0.7);
+		padding: 0px 6px;
+	}
 
-    .inner-frame-disabled {
-        background-color: rgba(0, 0, 0, 0);
-    }
+	.inner-frame-disabled {
+		background-color: rgba(0, 0, 0, 0);
+	}
 
-    .flex-icon-text {
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-    }
+	.flex-icon-text {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
 
-    .pack-icon {
-        max-width: 20px;
-        max-height: 20px;
-        padding: 4px 12px 4px 0px;
-    }
+	.pack-icon {
+		max-width: 20px;
+		max-height: 20px;
+		padding: 4px 12px 4px 0px;
+	}
 
-    .text {
-        font-weight: bold;
-    }
+	.text {
+		font-weight: bold;
+		font-size: 0.9em;
+	}
 
-    .sub-text {
-        font-size: x-small;
-        text-decoration: none;
-    }
+	.sub-text {
+		font-size: x-small;
+		text-decoration: none;
+	}
 
-    .disabled-text {
-        color: rgb(147, 147, 147);
-    }
+	.disabled-text {
+		color: rgb(147, 147, 147);
+	}
 </style>
