5c4298c780ed90ea0602765c5dd248ae219f7351
Sirawat Pitaksarit <pyasry@gmail.com>
2024-12-06 19:00:25 +0700

    better link coloring

--- arkham-starter-sveltekit/src/lib/explore/ExploreLinkTable.svelte
+++ arkham-starter-sveltekit/src/lib/explore/ExploreLinkTable.svelte
@@ -10,6 +10,23 @@
 	export let popupDatabase: PopupDatabase
 	export let fullDatabase: FullDatabase
 
+	function hashString(str: string): number {
+		let hash = 0
+		for (let i = 0; i < str.length; i++) {
+			const char = str.charCodeAt(i)
+			hash = (hash << 5) - hash + char
+			hash |= 0 // Convert to 32bit integer
+		}
+		return hash
+	}
+
+	function assignRandomColorToSite(site: string): string {
+		const baseColor = 200 // Base hue value
+		const hashValue = Math.abs(hashString(site))
+		const hueShift = hashValue % 360 // Ensure hue shift is within 0-359
+		return `hsl(${(baseColor + hueShift) % 360}, 70%, 25%)`
+	}
+
 	function linkCardsToEntries(cards: string[]): DecklistEntry[] {
 		return cards.map<DecklistEntry>((x) => {
 			return {
@@ -21,59 +38,83 @@
 	}
 </script>
 
-<table>
-	<thead>
-		<tr>
-			<th class="site">Site</th>
-			<th class="title">Title</th>
-			<th></th>
-			<th>Link</th>
-		</tr>
-	</thead>
-	<tbody>
-		{#each links as link}
-			<tr>
-				<td class="site">{link.site}</td>
-				<td>{link.name}</td>
-				<td>
-					{#if link.cards !== undefined}
-						<CardLaid
-							entries={linkCardsToEntries(link.cards)}
-							groupings={[]}
-							sortings={[]}
-							{fullDatabase}
-							{popupDatabase}
-							toggleMap={{}}
-							small
-						/>
-					{/if}
-				</td>
-				<td class="link"
-					><a href={link.url} target="_blank" rel="noopener noreferrer">{link.url}</a></td
-				>
-			</tr>
-		{/each}
-	</tbody>
-</table>
+<div class="link-list">
+	{#each links as link}
+		{@const formattedDate = link.date !== undefined ? new Date(link.date).toLocaleDateString() : ''}
+		<div class="link-item">
+			<a href={link.url} target="_blank" rel="noopener noreferrer">
+				<div class="site" style="color: {assignRandomColorToSite(link.site)};">
+					<span>{link.site}</span>
+					<span class="date">{formattedDate}</span>
+				</div>
+				<div class="title">{link.name}</div>
+				<div class="link">
+					{link.url}
+				</div>
+			</a>
+			{#if link.cards !== undefined}
+				<div class="cards">
+					<CardLaid
+						entries={linkCardsToEntries(link.cards)}
+						groupings={[]}
+						sortings={[]}
+						{fullDatabase}
+						{popupDatabase}
+						toggleMap={{}}
+						small
+					/>
+				</div>
+			{/if}
+		</div>
+	{/each}
+</div>
 
 <style>
-	table {
-		width: 100%;
-		border-collapse: collapse;
+	.link-list {
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
 	}
-	.site {
-		font-weight: bold;
-	}
-	tr {
+
+	.link-item {
+		padding: 1em;
 		border: solid;
 		border-color: rgba(0, 0, 0, 0.1);
 		border-width: 1px 0;
 	}
-	td {
-		padding: 1em;
+
+	.site {
+		font-weight: bold;
+	}
+
+	.title {
+		font-size: 1.2em;
+		font-weight: bold;
+	}
+
+	.date {
+		font-size: x-small;
 	}
 
 	.link {
+		font-size: x-small;
+	}
+
+	.link a {
 		line-break: anywhere;
+		text-decoration: none;
+		color: inherit;
+	}
+
+	.cards {
+		margin-top: 0.5em;
+	}
+
+	dt {
+		font-weight: bold;
+	}
+
+	dd {
+		margin: 0 0 1em 0;
 	}
 </style>
